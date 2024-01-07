d64121f18f3ee5b97d6c5393548b57052b389b15
wanissha <65160353@my.buu.ac.th>
2024-01-07 16:14:21 +0700

    Revert "example-app"
    This reverts commit 52046f2a4d3c088264715d2a15ad1565dcca9e67.

--- example-app/storage/framework/views/3c6a95a1e4d2e94b0d3f1e9232946c5f.php
+++
@@ -1,9 +0,0 @@
-<h1>Hello world</h1>
-<h1> <?php echo $val_a . ''. $val_b; ?></h1>
-<h1><?php echo e($val_a  . ' ' .  $val_b); ?></h1>
-<form action="" method="post">
-    <?php echo csrf_field(); ?>
-    <input type="text" name=""myinput>
-    <button type="submit" name="submit">Submit</button>
-</form>
-<?php /**PATH C:\xampp\htdocs\SE_CAMP\example-app\resources\views/mypage.blade.php ENDPATH**/ ?>
